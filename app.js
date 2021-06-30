b0afb546983a4180c3223b4fa428b7cff790d7be
Daehwan2 <dhdh0482@naver.com>
2021-06-30 23:40:38 +0900

    배포수정

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const postRouter = require('./routes/post');
 const deleteRouter = require('./routes/delete');
 const editRouter = require('./routes/edit');
 const {sequelize} = require('./models');
+const logger = require('./logger');
+const helmet = require('helmet');
+const hpp = require('hpp');
 
 const app = express();
 app.set('port',process.env.PORT || 8001);
@@ -28,13 +31,18 @@ sequelize.sync({force:false})
 .catch((err)=>{
     console.error(err);
 });
-
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'production'){
+    app.use(morgan('combined'));
+    app.use(helmet({contentSecurityPolicy:false}));
+    app.use(hpp());
+}else{
+    app.use(morgan('dev'));
+}
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionOption ={
     resave:false,
     saveUninitialized:false,
     secret:process.env.COOKIE_SECRET,
@@ -42,7 +50,12 @@ app.use(session({
         httpOnly:true,
         secure:false,
     },
-}));
+};
+if(process.env.NODE_ENV === 'production'){
+    sessionOption.proxy = true;
+    //sessionOption.cookie.secure=true;
+}
+app.use(session(sessionOption));
 
 app.use('/',pageRouter);
 app.use('/post',postRouter);
@@ -52,6 +65,8 @@ app.use('/edit',editRouter);
 app.use((req,res,next)=>{
     const error = new Error(`${req.method} ${req.url}라우터가 없습니다.`);
     error.status =404;
+    logger.info('hello');
+    logger.error(error.message);
     next(error);
 });
 
